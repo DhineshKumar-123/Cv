ec3088d98545947fd8b4653b70a0a2aa6d6b6219
M. Dhinesh Kumar <132334064+DhineshKumar-123@users.noreply.github.com>
2023-11-06 14:13:38 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import InputPage from './PDFGenerator';
+import PDFGenerator from './PDFGenerator';
+
+function App() {
+  return (
+    <div>
+      <h1>PDF Generation Example</h1>
+      <PDFGenerator/>
+    </div>
+  );
+}
+
+export default App;
